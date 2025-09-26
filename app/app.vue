92d2c952698aa4a90315e94ff7d67c9480ad7a8b
Kristian Kalvå <kristian@ensign.no>
2025-09-26 09:41:46 +0200

    Cleanup

--- app/app.vue
+++ app/app.vue
@@ -4,6 +4,7 @@
     <NuxtPage />
     <footer class="c-horizontal-list">
       <nuxt-link to="/">Home</nuxt-link>
+      <nuxt-link to="file">Files</nuxt-link>
       <nuxt-link to="project">Projects</nuxt-link>
     </footer>
   </div>
---
+++ app/pages/file/index.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { useFilesStore } from '@/stores/files';
+import { storeToRefs } from 'pinia';
+
+/** Pinia store for file management. */
+const filesStore = useFilesStore();
+const { files } = storeToRefs(filesStore);
+
+const numberOfFiles = computed(() => {
+  return files.value.length;
+});
+</script>
+
+<template>
+  <section class="c-section c-section--main c-section--gap">
+    <h1>Upload files</h1>
+    <file-drop-zone />
+    <div v-if="numberOfFiles">
+      <h3>{{ numberOfFiles }} files</h3>
+      <file-list />
+      <hr />
+      <file-parse-button />
+    </div>
+  </section>
+</template>
