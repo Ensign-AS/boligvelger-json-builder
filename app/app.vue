3a5532664b302ea08a2030d75cdce9335571b4b8
Kristian Kalvå <kristian@ensign.no>
2025-09-26 09:13:27 +0200

    Added a debug footer

--- app/app.vue
+++ app/app.vue
@@ -2,5 +2,9 @@
   <div>
     <TheHeader />
     <NuxtPage />
+    <footer class="c-horizontal-list">
+      <nuxt-link to="/">Home</nuxt-link>
+      <nuxt-link to="project">Projects</nuxt-link>
+    </footer>
   </div>
 </template>
