783c04fe7bb541f55ffa4e3c1f3efbb32ca4951f
Kristian Kalvå <kristian@ensign.no>
2025-11-24 11:06:02 +0100

    Added PWA things

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,4 +22,38 @@ export default defineNuxtConfig({
     storage: 'localStorage',
     storageKey: 'color-mode',
   },
+  pwa: {
+    injectRegister: 'auto',
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Boligvelger JSON Builder',
+      short_name: 'BV JSON',
+      description: 'JSON generator for Boligvelger',
+      theme_color: '#ffffff',
+      orientation: 'any',
+      dir: 'ltr',
+      categories: ['productivity'],
+      scope: '/',
+      icons: [
+        {
+          src: '/icon-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+      start_url: '/',
+      display: 'standalone',
+      background_color: '#ffffff',
+      lang: 'nb',
+      id: 'boligvelger-json-builder',
+    },
+    injectManifest: {
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+    },
+  },
 });
